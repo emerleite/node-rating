589705b5f6027cfc37beacf31ed8247735c6772a
Emerson Macedo <emerleite@gmail.com>
2011-01-29 21:01:24 -0200

    Added rate model to database cleanner

--- test/helper/database-cleaner.js
+++ test/helper/database-cleaner.js
@@ -4,7 +4,9 @@ var mongoose = require('mongoose').Mongoose
 
 exports.clean = function () {
     var db = mongoose.connect('mongodb://localhost/rating_test'),
-        Hit = require('hit').Hit(db);
+        Hit = require('hit').Hit(db),
+        Rate = require('rate').Rate(db);
 
     Hit.remove({}, function () {});
+    Rate.remove({}, function () {});
 }
