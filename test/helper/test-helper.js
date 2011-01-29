2ee9fc6e8a7479f44df84cce494db020838b1ff1
Emerson Macedo <emerleite@gmail.com>
2011-01-29 20:36:33 -0200

    Allow request with cookies

--- test/helper/test-helper.js
+++ test/helper/test-helper.js
@@ -6,8 +6,13 @@ require.paths.unshift('lib');
 require.paths.unshift('vendor/mongoose');
 
 exports.makeRequest = function (params, callback) {
+    var headers = {'host': params.host +':' + params.port, 'Content-Type': params.headers.contentType, 'Content-length': params.body.length};
+
+    if (params.headers.cookie) { headers['Cookie'] = params.headers.cookie; }
+
     var client = http.createClient(params.port, params.host);
-    var request = client.request(params.method, params.uri, {'host': params.host +':' + params.port, 'Content-Type': params.headers.contentType, 'Content-length': params.body.length}); 
+    var request = client.request(params.method, params.uri, headers); 
+
     request.on('response', callback);
     request.end(params.body, 'utf8');
 };
