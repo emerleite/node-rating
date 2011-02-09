29c4da5802bb4aca856ba173602fa5376b1d0dbd
Emerson Macedo <emerleite@gmail.com>
2011-02-09 01:01:41 -0200

    Database Cleanner now cleans all collections

--- test/helper/database-cleaner.js
+++ test/helper/database-cleaner.js
@@ -3,10 +3,10 @@ require('./test-helper');
 var mongoose = require('mongoose');
 
 exports.clean = function () {
-  var db = mongoose.createConnection('mongodb://localhost/rating_test'),
-  Hit = require('hit').Hit(db),
-  Rate = require('rate').Rate(db);
-  
-  Hit.remove({}, function () {});
-  Rate.remove({}, function () {});
-}
+  var db = mongoose.createConnection('mongodb://localhost/rating_test').db;
+  db.collections( function (skip, collections) {
+    collections.forEach(function (collection) {
+      collection.remove({}, function () {});
+    });
+  });
+};
