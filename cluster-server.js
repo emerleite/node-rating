1ed6b9a445ce92fc11547d966f85f0c6d5761d3c
Emerson Macedo <emerleite@gmail.com>
2011-03-11 14:26:31 -0300

    listen needs an integer to define port, so needs parseInt

--- cluster-server.js
+++ cluster-server.js
@@ -2,8 +2,9 @@ var server = require('./node-rating');
 var port = process.env.PORT || "3000";
 var cluster = require('cluster');
 cluster(server)
+  .set('workers', 1)
   .use(cluster.logger())
   .use(cluster.stats())
   .use(cluster.pidfiles())
   .use(cluster.cli())
-  .listen(port);
+  .listen(parseInt(port));
